e2839a24e76ad74b6f415479e3f65b1aa79e1dc8
damian <damiangalvan66@gmail.com>
2021-11-29 09:26:00 -0600

    refactor _id to id

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -4,12 +4,12 @@ import { Card } from 'react-bootstrap'
 export const Product = ({ product }) => {
     return (
         <Card className='my-3 p-3 rounded'>
-            <a href={`/product/${product._id}`}>
+            <a href={`/product/${product.id}`}>
                 <Card.Img src={product.image} variant='top' />
             </a>
 
             <Card.Body>
-                <a href={`/product/${product._id}`}>
+                <a href={`/product/${product.id}`}>
                     <Card.Title as='div'>
                         <strong>{product.name}</strong>
                     </Card.Title>
