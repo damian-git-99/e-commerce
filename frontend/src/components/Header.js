325252c77c1b8adfcf046b77d0144ea208c63ebf
damian <damiangalvan66@gmail.com>
2021-11-29 08:53:15 -0600

    add Header component

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+
+export const Header = () => {
+    return (
+        <header>
+            <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+                <Container>
+                    <Navbar.Brand href="/">Shop</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                        <Nav className="ml-auto">
+                            <Nav.Link href="/cart"><i className="fas fa-shopping-cart"></i> Cart</Nav.Link>
+                            <Nav.Link href="/login"><i className="fas fa-user"></i> Sign In</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </header>
+    )
+}
