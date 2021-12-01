603be26ab898f2d22e43c1c3e4badd9a5837fae3
damian <damiangalvan66@gmail.com>
2021-12-01 17:46:53 -0600

    add user model

---
+++ backend-nodejs/modules/user/model/UserModel.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        required: true,
+        default: false
+    }
+}, {
+    timestamps: true
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+module.exports = UserModel;
