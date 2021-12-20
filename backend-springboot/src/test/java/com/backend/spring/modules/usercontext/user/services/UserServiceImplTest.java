0473d225e3799ebb2e66a0f3d922134fed532b8f
damian <damiangalvan66@gmail.com>
2021-12-20 16:45:44 -0600

    add tests

--- backend-springboot/src/test/java/com/backend/spring/modules/usercontext/user/services/UserServiceImplTest.java
+++ backend-springboot/src/test/java/com/backend/spring/modules/usercontext/user/services/UserServiceImplTest.java
@@ -7,16 +7,21 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
+import org.mockito.stubbing.Answer;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atMostOnce;
@@ -27,6 +32,8 @@ class UserServiceImplTest {
 
     @Mock
     private UserDao userDao;
+    @Mock
+    private PasswordEncoder passwordEncoder;
 
     @InjectMocks
     private UserServiceImpl userService;
@@ -68,4 +75,29 @@ class UserServiceImplTest {
         assertThat(exception.getMessage()).isEqualTo("User not found");
         then(userDao).should(atMostOnce()).findUserByEmail("damian@gmail.com");
     }
+
+    @Test
+    @DisplayName("should save an user")
+    void shouldSaveUser() {
+        User userData = new User("Damian", "damian@gmail.com", "123456");
+        given(passwordEncoder.encode(anyString())).willReturn("encrypted password");
+        given(userDao.save(any(User.class))).will(new Answer<User>() {
+            Long cont = 10L;
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+                User user1 = invocationOnMock.getArgument(0);
+                user1.setId(cont++);
+                return user1;
+            }
+        });
+
+        User user = userService.save(userData);
+
+        assertThat(user.getId()).isNotNull().isEqualTo(10L);
+        assertThat(user.getPassword()).isEqualTo("encrypted password");
+        then(userDao).should(atMostOnce()).save(any(User.class));
+        then(passwordEncoder).should(atMostOnce()).encode(anyString());
+    }
+
 }
