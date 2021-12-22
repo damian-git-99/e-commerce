c0acad9648611dc7535b68fcffcfd1997c504706
damian <damiangalvan66@gmail.com>
2021-12-22 17:46:14 -0600

    add new tests

--- backend-springboot/src/test/java/com/backend/spring/modules/usercontext/user/services/UserServiceImplTest.java
+++ backend-springboot/src/test/java/com/backend/spring/modules/usercontext/user/services/UserServiceImplTest.java
@@ -1,7 +1,10 @@
 package com.backend.spring.modules.usercontext.user.services;
 
+import com.backend.spring.modules.usercontext.role.Role;
+import com.backend.spring.modules.usercontext.role.RoleDao;
 import com.backend.spring.modules.usercontext.user.daos.UserDao;
 import com.backend.spring.modules.usercontext.user.entities.User;
+import com.backend.spring.shared.exceptions.CustomException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,6 +28,7 @@ import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.atMostOnce;
+import static org.mockito.Mockito.never;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -32,6 +36,8 @@ class UserServiceImplTest {
 
     @Mock
     private UserDao userDao;
+    @Mock
+    RoleDao roleDao;
     @Mock
     private PasswordEncoder passwordEncoder;
 
@@ -98,6 +104,62 @@ class UserServiceImplTest {
         assertThat(user.getPassword()).isEqualTo("encrypted password");
         then(userDao).should(atMostOnce()).save(any(User.class));
         then(passwordEncoder).should(atMostOnce()).encode(anyString());
+    }
+
+    @Test
+    @DisplayName("should register a user successfully")
+    void shouldSignUp(){
+        User userData = new User("Damian", "damian@gmail.com", "123456");
+        given(userDao.findUserByEmail("damian@gmail.com")).willReturn(Optional.empty());
+        given(roleDao.findByName("ROLE_USER")).willReturn(Optional.of(new Role("ROLE_USER")));
+        given(userDao.save(any(User.class))).will(new Answer<User>() {
+            Long cont = 10L;
+
+            @Override
+            public User answer(InvocationOnMock invocationOnMock) throws Throwable {
+                User user1 = invocationOnMock.getArgument(0);
+                user1.setId(cont++);
+                return user1;
+            }
+        });
+
+        User user = userService.signup(userData);
+        assertThat(user.getId()).isEqualTo(10L);
+        assertThat(user.getRoles()).isNotEmpty().hasSize(1);
+        then(userDao).should(atMostOnce()).findUserByEmail(anyString());
+        then(roleDao).should(atMostOnce()).findByName(anyString());
+    }
+
+    @Test
+    @DisplayName("should fail trying to register a user")
+    void shouldFailSignUp(){
+        User userData = new User("Damian", "damian@gmail.com", "123456");
+        given(userDao.findUserByEmail("damian@gmail.com")).willReturn(Optional.of(userData));
+        given(roleDao.findByName("ROLE_USER")).willReturn(Optional.of(new Role("ROLE_USER")));
+
+        Exception exception = assertThrows(CustomException.class, () -> {
+            User user = userService.signup(userData);
+        });
+
+        assertThat(exception.getClass()).isEqualTo(CustomException.class);
+        assertThat(exception.getMessage()).isEqualTo("Email already exists");
+        then(roleDao).should(never()).findByName(anyString());
+        then(userDao).should(never()).save(any(User.class));
+    }
+
+    @Test
+    @DisplayName("should update a user")
+    void shouldUpdateUser(){
+        User user = new User("Damian", "damian@gmail.com", "123456");
+        User newUser = new User("Damian actualizado", "damian@gmail.com actualizado", "123456");
+        given(userDao.save(any(User.class))).willReturn(user);
+
+        User updatedUser = userService.updateUser(newUser, user);
+
+        assertThat(updatedUser.getName()).isEqualTo("Damian actualizado");
+        assertThat(updatedUser.getEmail()).isEqualTo("damian@gmail.com actualizado");
+        then(userDao).should(atMostOnce()).save(any(User.class));
+
     }
 
 }
