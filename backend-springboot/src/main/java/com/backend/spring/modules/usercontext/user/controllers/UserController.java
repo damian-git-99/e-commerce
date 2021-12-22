8b6bb91d1920ad3f368d3f996d39893ef672e2cc
damian <damiangalvan66@gmail.com>
2021-12-22 16:39:50 -0600

    add new methods

--- backend-springboot/src/main/java/com/backend/spring/modules/usercontext/user/controllers/UserController.java
+++ backend-springboot/src/main/java/com/backend/spring/modules/usercontext/user/controllers/UserController.java
@@ -1,16 +1,21 @@
 package com.backend.spring.modules.usercontext.user.controllers;
 
+import com.backend.spring.modules.usercontext.user.dtos.UserDtoToken;
+import com.backend.spring.modules.usercontext.user.dtos.UserDtoTokenConverter;
 import com.backend.spring.modules.usercontext.user.dtos.UserProfileDTO;
 import com.backend.spring.modules.usercontext.user.dtos.UserProfileDTOConverter;
 import com.backend.spring.modules.usercontext.user.entities.User;
 import com.backend.spring.modules.usercontext.user.services.UserService;
+import com.backend.spring.shared.BindingResultUtils;
+import com.backend.spring.shared.exceptions.CustomException;
 import com.backend.spring.shared.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.security.Principal;
 
 @RestController
@@ -20,10 +25,19 @@ public class UserController {
     private UserService userService;
     @Autowired
     private UserProfileDTOConverter userProfileDTOConverter;
+    @Autowired
+    private UserDtoTokenConverter userDtoTokenConverter;
 
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
+    }
+
+    @PostMapping("/signup")
+    public UserDtoToken signUp(@RequestBody @Valid User user, BindingResult result){
+        if (result.hasErrors()) BindingResultUtils.returnBindingErrors(result);
+        User newUser = userService.signup(user);
+        return userDtoTokenConverter.toDTO(newUser);
     }
 
     @Secured("ROLE_USER")
@@ -31,6 +45,14 @@ public class UserController {
     public UserProfileDTO getProfile(Principal principal) {
         User user = userService.findByEmail(principal.getName()).orElseThrow(() -> new ResourceNotFoundException("user not found"));
         return userProfileDTOConverter.toDTO(user);
+    }
+
+    @Secured("ROLE_USER")
+    @PutMapping("/profile")
+    public UserDtoToken updateUser(@RequestBody User newUser, Principal principal){
+        User user = userService.findByEmail(principal.getName()).orElseThrow(() -> new CustomException("User not found in session", HttpStatus.UNAUTHORIZED));
+        User updatedUser = userService.updateUser(newUser, user);
+        return userDtoTokenConverter.toDTO(updatedUser);
     }
 
 }
