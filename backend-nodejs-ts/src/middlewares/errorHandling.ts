baab0752053e665cefb45afc03f386b937c0f513
damian <damiangalvan66@gmail.com>
2022-09-28 12:04:05 -0500

    delete typescript project

--- backend-nodejs-ts/src/middlewares/errorHandling.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction} from 'express';
-
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  res.status(statusCode).json({
-    message: err.message,
-  });
-};
--- backend-nodejs-ts/src/utils/generateToken.ts
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-export const generateToken = (payload: any) => {
-  const KEY = process.env.JWT_SECRET || '';
-  const token = jwt.sign({ ...payload }, KEY, { expiresIn: '2h' });
-  return token;
-};
