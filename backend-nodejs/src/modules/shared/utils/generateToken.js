5d35946b08157c2cb53b4b5a7d60a4e01417ab0c
damian <damiangalvan66@gmail.com>
2022-09-29 13:40:28 -0500

    add documentation

--- backend-nodejs/src/modules/shared/utils/generateToken.js
+++ backend-nodejs/src/modules/shared/utils/generateToken.js
@@ -1,6 +1,14 @@
 const jwt = require('jsonwebtoken');
+const config = require('config');
+const KEY = config.get('JWT_SECRET');
 
-const generateToken = (payload, key = process.env.JWT_SECRET) => {
+/**
+ * It takes a payload and a key, and returns a JWT token.
+ * @param payload - The data you want to store in the token.
+ * @param [key] - The secret key used to sign the token.
+ * @returns A token
+ */
+const generateToken = (payload, key = KEY) => {
   const token = jwt.sign({ ...payload }, key, { expiresIn: '2h' });
   return token;
 };
