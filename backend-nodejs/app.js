0bbe00af6fc1094c119e5890c35b9a6368cc4d0a
damian <damiangalvan66@gmail.com>
2022-09-29 13:43:42 -0500

    refactor, moved app object from file to work well with tests

---
+++ backend-nodejs/app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const app = express();
+const { productRouter } = require('./src/modules/product/productRoutes');
+const { errorHandler } = require('./src/middlewares/errorHandlingMiddleware');
+const { userRouter } = require('./src/modules/user/userRoutes');
+const { orderRouter } = require('./src/modules/order/orderRoutes');
+const { uploadRoutes } = require('./src/modules/shared/uploadRoutes');
+const config = require('config');
+const PAYPAL_CLIENT_ID = config.get('PAYPAL_CLIENT_ID');
+
+app.use(morgan('combined'));
+app.use(express.json());
+app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+app.use('/api/orders', orderRouter);
+app.use('/api/upload', uploadRoutes);
+app.get('/api/config/paypal', (req, res) => {
+  res.send(PAYPAL_CLIENT_ID);
+});
+app.get('/api/config/veifytoken', (req, res) => {
+
+});
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use(errorHandler);
+
+module.exports = {
+  app
+};
--- backend-nodejs/server.js
+++ backend-nodejs/server.js
@@ -1,35 +1,11 @@
-const express = require('express');
-const path = require('path');
 const connectDB = require('./src/config/config');
+const config = require('config');
 // eslint-disable-next-line no-unused-vars
 const colors = require('colors');
-const { productRouter } = require('./src/modules/product/productRoutes');
-const { errorHandler } = require('./src/middlewares/errorHandlingMiddleware');
-const { userRouter } = require('./src/modules/user/userRoutes');
-const { orderRouter } = require('./src/modules/order/orderRoutes');
-const morgan = require('morgan');
-const { uploadRoutes } = require('./src/modules/shared/uploadRoutes');
-const app = express();
-const config = require('config');
+const { app } = require('./app');
 const PORT = config.get('port');
-const PAYPAL_CLIENT_ID = config.get('PAYPAL_CLIENT_ID');
 
 connectDB();
-app.use(morgan('combined'));
-app.use(express.json());
-app.use('/api/products', productRouter);
-app.use('/api/users', userRouter);
-app.use('/api/orders', orderRouter);
-app.use('/api/upload', uploadRoutes);
-app.get('/api/config/paypal', (req, res) => {
-  res.send(PAYPAL_CLIENT_ID);
-});
-app.get('/api/config/veifytoken', (req, res) => {
-
-});
-
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
-app.use(errorHandler);
 
 app.listen(PORT, () => {
   console.log(`Server running on port: ${PORT}`.blue.underline.bold);
