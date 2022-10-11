0128b98f98f14a40f47f69e8a477aa89a5cf3dd9
damian <damiangalvan66@gmail.com>
2022-10-11 10:12:50 -0500

    minor changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from 'react';
 import { Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
+import { useRouteMatch } from 'react-router-dom';
 import { listProducts } from '../actions/productActions';
 import { Loader } from '../components/Loader';
 import { Message } from '../components/Message';
 import { Product } from '../components/Product';
 
-// eslint-disable-next-line react/prop-types
-export const HomeScreen = ({ match }) => {
-  // eslint-disable-next-line react/prop-types
+export const HomeScreen = () => {
+  const match = useRouteMatch();
   const keyword = match.params.keyword;
   const dispatch = useDispatch();
   const productList = useSelector((state) => state.productList);
