97969fe166d664d8bf900e1fe6b7a4b07940e15e
damian <damiangalvan66@gmail.com>
2023-02-21 11:09:17 -0600

    add jwt validation for paypal token

--- backend-nodejs/src/modules/paypal/getToken.js
+++ backend-nodejs/src/modules/paypal/getToken.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
 const config = require('config');
+const { validateJwt } = require('../../shared/middlewares/validateJWT');
 const PAYPAL_CLIENT_ID = config.get('PAYPAL_CLIENT_ID');
 const app = Router();
 
-app.get('/api/config/paypal', (req, res) => {
+app.get('/api/config/paypal', validateJwt, (req, res) => {
   res.send(PAYPAL_CLIENT_ID);
 });
 
