ea1041ec5403590f18f8335785b1265964901722
damian <damiangalvan66@gmail.com>
2023-05-13 09:30:10 -0600

    refactor: fix bug

--- backend-nodejs/src/app.js
+++ backend-nodejs/src/app.js
@@ -11,7 +11,7 @@ const paypalRoutes = require('./paypal/getToken');
 const app = express();
 
 const corsOptions = {
-  origin: 'http://localhost:3000',
+  origin: 'http://localhost:5173',
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 };
 app.use(cors(corsOptions));
