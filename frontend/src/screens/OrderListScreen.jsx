a4aa022f8c5da9d236408cdc23ed5daa34bd9573
damian <damiangalvan66@gmail.com>
2023-05-16 09:45:15 -0600

    refactor(orderAPI.js, OrderListScreen.jsx, UserListScreen.jsx): replace redux actions with api calls and update components to use them

--- frontend/src/screens/OrderListScreen.jsx
+++ frontend/src/screens/OrderListScreen.jsx
@@ -1,89 +1,88 @@
-/* eslint-disable react/prop-types */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
-import { useDispatch, useSelector } from 'react-redux';
-import { listOrders } from '../actions/orderActions';
 import { Message } from '../components/Message';
 import { Loader } from '../components/Loader';
 import { useUserInfo } from '../hooks/useUserInfo';
+import { getOrders } from '../api/orderAPI';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
-export const OrderListScreen = ({ history }) => {
-  const dispatch = useDispatch();
+export const OrderListScreen = () => {
+  const history = useHistory();
+  const [orders, setOrders] = useState([]);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
   const { userLogin } = useUserInfo();
-  const orderList = useSelector((state) => state.orderList);
-  const { loading, error, orders } = orderList;
   const { userInfo } = userLogin;
 
   useEffect(() => {
     if (userInfo && userInfo.isAdmin) {
-      dispatch(listOrders());
+      setError(false);
+      setLoading(true);
+      getOrders(userInfo.token)
+        .then(data => {
+          setOrders(data);
+        })
+        .catch(error => setError(error.message))
+        .finally(() => setLoading(false));
     } else {
       history.push('/login');
     }
-  }, [dispatch, history, userInfo]);
+  }, [history, userInfo]);
 
   return (
     <>
       <h1>Orders</h1>
-      {loading
-        ? (
-        <Loader />
-          )
-        : error
-          ? (
-        <Message variant="danger">{error}</Message>
-            )
-          : (
-        <Table striped bordered hover responsive className="table-sm">
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>USER</th>
-              <th>DATE</th>
-              <th>TOTAL</th>
-              <th>PAID</th>
-              <th>DELIVERED</th>
-              <th></th>
+      {loading && <Loader />}
+      {error && <Message variant="danger">{error}</Message>}
+      <Table striped bordered hover responsive className="table-sm">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>USER</th>
+            <th>DATE</th>
+            <th>TOTAL</th>
+            <th>PAID</th>
+            <th>DELIVERED</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders.map((order) => (
+            <tr key={order.id}>
+              <td>{order.id}</td>
+              <td>{order.user && order.user.name}</td>
+              <td>{order.createdAt.substring(0, 10)}</td>
+              <td>${order.totalPrice}</td>
+              <td>
+                {order.isPaid
+                  ? (
+                      order.paidAt.substring(0, 10)
+                    )
+                  : (
+                  <i className="fas fa-times" style={{ color: 'red' }}></i>
+                    )}
+              </td>
+              <td>
+                {order.isDelivered
+                  ? (
+                      order.deliveredAt.substring(0, 10)
+                    )
+                  : (
+                  <i className="fas fa-times" style={{ color: 'red' }}></i>
+                    )}
+              </td>
+              <td>
+                <LinkContainer to={`/order/${order.id}`}>
+                  <Button variant="light" className="btn-sm">
+                    Details
+                  </Button>
+                </LinkContainer>
+              </td>
             </tr>
-          </thead>
-          <tbody>
-            {orders.map((order) => (
-              <tr key={order.id}>
-                <td>{order.id}</td>
-                <td>{order.user && order.user.name}</td>
-                <td>{order.createdAt.substring(0, 10)}</td>
-                <td>${order.totalPrice}</td>
-                <td>
-                  {order.isPaid
-                    ? (
-                        order.paidAt.substring(0, 10)
-                      )
-                    : (
-                    <i className="fas fa-times" style={{ color: 'red' }}></i>
-                      )}
-                </td>
-                <td>
-                  {order.isDelivered
-                    ? (
-                        order.deliveredAt.substring(0, 10)
-                      )
-                    : (
-                    <i className="fas fa-times" style={{ color: 'red' }}></i>
-                      )}
-                </td>
-                <td>
-                  <LinkContainer to={`/order/${order.id}`}>
-                    <Button variant="light" className="btn-sm">
-                      Details
-                    </Button>
-                  </LinkContainer>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-            )}
+          ))}
+        </tbody>
+      </Table>
     </>
   );
 };
