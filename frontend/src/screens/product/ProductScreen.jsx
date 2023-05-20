d7cd5c9eec691ccbc223dec90847d07ac430dcdb
damian <damiangalvan66@gmail.com>
2023-05-20 11:36:41 -0600

    refactor(ProductScreen.jsx): move Reviews component to its own file and import it fix(Reviews.jsx): fix eslint errors and warnings, add prop-types validation

--- frontend/src/screens/product/ProductScreen.jsx
+++ frontend/src/screens/product/ProductScreen.jsx
@@ -2,10 +2,10 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Row, Col, Image, ListGroup, Card, Button, Form, Alert } from 'react-bootstrap';
-import { addProductReview, getProductDetails } from '../../api/productsAPI';
 import { Loader } from '../../components/Loader';
 import { Rating } from '../../components/Rating';
-import { useUserInfo } from '../../hooks/useUserInfo';
+import { Reviews } from './Reviews';
+import { getProductDetails } from '../../api/productsAPI';
 
 export const ProductScreen = () => {
   const navigate = useNavigate();
@@ -134,89 +134,6 @@ export const ProductScreen = () => {
           </Row>
         </>
             )}
-    </>
-  );
-};
-
-export const Reviews = ({ product, setAddReview }) => {
-  const { userLogin } = useUserInfo();
-  const { userInfo } = userLogin;
-  const [rating, setRating] = useState(0);
-  const [comment, setComment] = useState('');
-  const [error, setError] = useState(undefined);
-  const match = useParams();
-
-  const submitHandler = (e) => {
-    e.preventDefault();
-    addProductReview(match.id, { rating, comment }, userInfo.token)
-      .then(_ => {
-        setAddReview(true);
-        setRating(0);
-        setComment('');
-      })
-      .catch(error => {
-        setError(error.message);
-      });
-  };
-
-  return (
-    <>
-      <h2>Reviews</h2>
-            {product && product.reviews && product.reviews.length === 0 && <Alert>No Reviews</Alert>}
-              <ListGroup variant='flush'>
-                {product && product.reviews && product.reviews.map((review) => (
-                  <ListGroup.Item key={review.id}>
-                    <strong>{review.name}</strong>
-                    <Rating value={review.rating} />
-                    <p>{review.createdAt.substring(0, 10)}</p>
-                    <p>{review.comment}</p>
-                  </ListGroup.Item>
-                ))}
-                <ListGroup.Item>
-                  <h2>Write a Customer Review</h2>
-                  {error && (
-                    <Alert variant='danger'>{error}</Alert>
-                  )}
-                  {/* todo: move to another component */}
-                  {userInfo
-                    ? (
-                    <Form onSubmit={submitHandler}>
-                      <Form.Group controlId='rating'>
-                        <Form.Label>Rating</Form.Label>
-                        <Form.Control
-                          as='select'
-                          value={rating}
-                          onChange={(e) => setRating(e.target.value)}
-                        >
-                          <option value=''>Select...</option>
-                          <option value='1'>1 - Poor</option>
-                          <option value='2'>2 - Fair</option>
-                          <option value='3'>3 - Good</option>
-                          <option value='4'>4 - Very Good</option>
-                          <option value='5'>5 - Excellent</option>
-                        </Form.Control>
-                      </Form.Group>
-                      <Form.Group controlId='comment'>
-                        <Form.Label>Comment</Form.Label>
-                        <Form.Control
-                          as='textarea'
-                          row='3'
-                          value={comment}
-                          onChange={(e) => setComment(e.target.value)}
-                        ></Form.Control>
-                      </Form.Group>
-                      <Button type='submit' variant='primary' className='mt-3'>
-                        Submit
-                      </Button>
-                    </Form>
-                      )
-                    : (
-                    <Alert>
-                      Please <Link to='/login'>sign in</Link> to write a review{' '}
-                    </Alert>
-                      )}
-                </ListGroup.Item>
-              </ListGroup>
     </>
   );
 };
